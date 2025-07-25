78e96f988c2630257c6c249aa8e1bd5f230f5518
energizer91 <militaryboy@yandex.ru>
2025-07-25 19:05:03 +0200

    feat: new icons and placeholder text

--- frontend/src/components/HolidayList.tsx
+++ frontend/src/components/HolidayList.tsx
@@ -1,6 +1,5 @@
 import type { HolidaysTypes } from 'date-holidays';
 import dayjs from 'dayjs';
-import { Card, CardContent } from '@/components/ui/card.tsx';
 
 interface HolidayListProps {
   data?: HolidaysTypes.Holiday[];
@@ -8,22 +7,16 @@ interface HolidayListProps {
 
 export const HolidayList = ({ data = [] }: HolidayListProps) => {
   return (
-    <Card>
-      <CardContent className="p-0">
-        <ul className="flex flex-col gap-2">
-          {data.map((event) => (
-            <li
-              key={event.name}
-              className="flex items-center justify-between px-4 hover:bg-muted-foreground/5"
-            >
-              <div className="text-sm text-muted-foreground">
-                {dayjs(event.date).format('DD MMMM')}
-              </div>
-              <div className="font-medium">{event.name}</div>
-            </li>
-          ))}
-        </ul>
-      </CardContent>
-    </Card>
+    <ul className="flex flex-col gap-2">
+      {data.map((event) => (
+        <li
+          key={event.name}
+          className="flex items-center justify-between px-4 hover:bg-muted-foreground/5"
+        >
+          <div className="text-sm text-muted-foreground">{dayjs(event.date).format('DD MMMM')}</div>
+          <div className="font-medium">{event.name}</div>
+        </li>
+      ))}
+    </ul>
   );
 };
